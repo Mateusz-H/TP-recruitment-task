e5d9a459ca0d7c62e268d38dee7cb24435cf4b92
Mateusz Hinca <mathew.b@bbod.io>
2021-04-05 16:45:08 +0200

    added tooltip in specializations

--- src/components/TherapistsList/TherpistsSpecializations.js
+++ src/components/TherapistsList/TherpistsSpecializations.js
@@ -1,10 +1,22 @@
-import { therapistsListSpan } from "../../styles/TherapistsListStyles/TherapistsListMain.module.scss";
+import {
+  therapistsListSpan,
+  tooltipText,
+  spanInList,
+} from "../../styles/TherapistsListStyles/TherapistsListMain.module.scss";
 export const TherapistsSpecializations = ({ id, specializations }) => {
   return (
-    <span className={therapistsListSpan}>
-      {specializations.slice(0,3).map((x) => {
-        return <span>{x}</span>;
+    <div className={therapistsListSpan}>
+      {specializations.length > 3 ? (
+        <span className={tooltipText}>
+          {specializations.map((x) => (
+            <span className={spanInList}>{x}</span>
+          ))}
+        </span>
+      ) : null}
+      {specializations.slice(0, 3).map((x) => {
+        return <span className={spanInList}>{x}</span>;
       })}
-    </span>
+        {specializations.length>3?"...":null}
+    </div>
   );
 };
